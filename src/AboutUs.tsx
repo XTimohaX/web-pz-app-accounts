c49e88ae7850b43b187692d61001b1b65f811dfe
Timoha <xtvv2020x@gmail.com>
2023-11-21 23:58:32 +0200

    PZAccounts

---
+++ src/AboutUs.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Header from "./Components/Header";
+
+const AboutUs = () => {
+
+
+
+
+    return (
+        <div className="body">
+        <Header />
+            <div className="block">
+
+                <div className="menu">
+                   <p>Lorem</p>
+
+
+                    <p>  Amet animi at  </p>
+
+                </div>
+                <div className="text_Content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium asperiores cum delectus eos et explicabo, inventore libero mollitia quibusdam rem reprehenderit temporibus. Accusantium ad, beatae consectetur eum facere fugit inventore itaque laborum magni necessitatibus officia, officiis quasi rerum sed sit temporibus vitae voluptates! Alias aliquam commodi culpa cupiditate delectus deleniti deserunt dolore dolorem doloremque doloribus eligendi et ex expedita hic illo impedit in ipsum maxime minima mollitia nisi nulla odit perferendis placeat porro possimus quidem recusandae saepe similique sunt ullam, unde velit voluptatum? Enim, repellat, veritatis. Amet animi at, atque cum cumque deserunt dicta dolorum enim error expedita facere fuga illo impedit, in laudantium libero minus, nam necessitatibus perspiciatis possimus qui quis voluptas voluptate. A adipisci asperiores consectetur consequuntur dignissimos ducimus ea eveniet hic iure molestiae, mollitia necessitatibus neque nihil officiis pariatur perferendis placeat quaerat quam quibusdam ratione saepe soluta vel? Eaque eos eveniet libero magnam recusandae reiciendis soluta?</div>
+            </div>
+
+        </div>
+    );
+};
+
+export default AboutUs;
